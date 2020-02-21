271b56e8532f3408c16177a381c33938a9450b88
Chaitanya Vura <vurachaitanya@users.noreply.github.com>
2020-02-21 21:34:22 +0530

    different route for different pages

---
+++ API_Routes.py
@@ -0,0 +1,15 @@
+## Usage :  python API_Routes.py & ; curl http://127.0.0.1:5000/ ; curl http://127.0.0.1:5000/login
+
+import flask
+
+app = flask.Flask(__name__)
+#app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def home():
+    return "Welcome to API World"
+
+@app.route('/login', methods=['GET'])
+def login():
+    return "Welcome to API login"
+app.run(debug=True)
